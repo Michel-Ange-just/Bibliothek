b001b41083a2c11f32281421282c0008b4af65d5
Michel-Ange-just <michelange.njile@gmail.com>
2026-03-14 22:48:49 +0100

    add of an new instance variable that says how many Books of this isbn there is

--- src/model/Book.java
+++ src/model/Book.java
@@ -7,15 +7,21 @@ public abstract class Book {
     private String title = null;
     private Integer erscheinungsJahr = null;
     private String verlag = null;
+    private Integer anzahl = 0;
 
 
-    public Book(String isbn, String autor, String title, Integer erscheinungsJahr, String verlag) {
+    public Book(String isbn, String autor, String title, Integer erscheinungsJahr, String verlag, Integer Anzahl) {
         this.isbn = isbn;
         this.autor = autor;
         this.title = title;
         this.erscheinungsJahr = erscheinungsJahr;
         this.verlag = verlag;
+        this.anzahl = anzahl;
     }
+    public Integer getAnzahl(){return this.anzahl; }
+    public void incrementBook(){this.anzahl++;}
+    public void decrementBook(){this.anzahl--;}
+
 
     public Book(String isbn){
         this.isbn = isbn;

1840440b04bd2e2fbc99c8d1a495fdff0b9750ed
Michel-Ange-just <michelange.njile@gmail.com>
2026-03-15 12:02:22 +0100

    add of other instance variables for a better usability

--- src/model/Loan.java
+++ src/model/Loan.java
@@ -1,13 +1,27 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Loan {
     private final String loanId;
     private final User user;
-    private final String isbn;
+    private final Book book;
+    private final LocalDate loanDate;
+    private final LocalDate dateToReturnBook;
+    private static int numberOfLoan = 0;
 
-    public Loan(String loanId, User user, String isbn) {
+    public Loan(String loanId, User user, Book book, LocalDate loanDate, LocalDate dateToReturnBook) {
         this.loanId = loanId;
         this.user = user;
-        this.isbn = isbn;
+        this.book = book;
+        this.loanDate = loanDate;
+        this.dateToReturnBook = dateToReturnBook;
+        numberOfLoan++;
+    }
+
+    public static int getNumberOfLoan(){return numberOfLoan;}
+
+    public String getLoanId() {
+        return loanId;
     }
 }

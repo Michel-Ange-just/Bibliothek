e5e96a4a89ef291177fc573140c0db2a833418f6
Michel-Ange-just <michelange.njile@gmail.com>
2026-03-16 10:22:22 +0100

    change of the storage Model

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -2,51 +2,86 @@ package repository;
 
 import exception.BookException;
 import model.Book;
-import util.Tuple;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BookRepository {
-    private final HashMap<Book,Integer> books;
+    private final HashMap<String,Book> books; //der erster Eintrag speichert die isbn und der zweiter das entsprechende Buch
 
     public BookRepository() {
         this.books = new HashMap<>();
     }
 
     public void addBook(Book book){
-        if(!books.containsKey(book)){
-            books.put(book,1);
+        if(!books.containsKey(book.getIsbn())){
+            books.put(book.getIsbn(), book);
+            System.out.println("book added");
         }
         else{
             book.incrementBook();
-            books.put(book,books.get(book)+1);
+            books.put(book.getIsbn(), book);
+            System.out.println("book one more time added");
         }
     }
 
-    public Integer removeBook(Book book){
-        return books.remove(book);
+    public void oneBookFree(Book book){
+        books.get(book.getIsbn()).incrementBook();
+    }
+
+    public void oneBookFree(String isbn) throws BookException {
+        books.get(isbn).incrementBook();
+    }
+
+    public void oneBookLess(Book book){
+        books.get(book.getIsbn()).decrementBook();
+    }
+
+    public void oneBookLess(String isbn) throws BookException {
+        books.get(isbn).decrementBook();
+    }
+
+
+    public void removeAllBook(Book book){
+        books.clear();
+        System.out.println("All books removed");
+    }
+
+    public void removeBook(Book book) throws BookException {
+        if (isBookExists(book)) {
+            book.decrementBook();
+            System.out.println("book removed");
+        }
+        throw new BookException("This book does not exist");
     }
 
     public boolean isBookExists(Book book){
-        return books.containsKey(book);
+        return books.containsKey(book.getIsbn());
     }
 
     public boolean isBookExists(String isbn){
-        for(Book book:books.keySet()){
-            if(book.getIsbn().equals(isbn)){
-                return true;
-            }
-        }
-        return false;
+        return books.containsKey(isbn);
     }
 
-    public Book getBook(String isbn) throws BookException { //get the book corresponding to the isbn
-        for(Book book:books.keySet()){
-            if(book.getIsbn().equals(isbn)){
+    public Book findByIsbn(String isbn) throws BookException { //get the book corresponding to the isbn
+        if(books.containsKey(isbn)){
+            return books.get(isbn);
+        }
+        throw new BookException("This book does not exist");
+    }
+
+    public Book findByTitle(String title) throws BookException {
+        for(Book book : books.values()){
+            if(book.getTitle().equals(title)){
                 return book;
             }
         }
+
         throw new BookException("This book does not exist");
+    }
+
+    public void printBookList(){
+        for(Book book : books.values()){
+            System.out.println(book.getTitle());
+        }
     }
 }
